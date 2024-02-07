cde7915ab8666bc997c537e02c2ae7825bc6fb1b
rravelli <remi.ravelli@gmail.com>
2024-02-07 16:49:05 +0100

    feat: add listener

---
+++ src/markov_listener.py
@@ -0,0 +1,19 @@
+from gramListener import gramListener
+
+
+class MarkovListener(gramListener):
+
+    def __init__(self):
+        pass
+
+    def enterDefstates(self, ctx):
+        pass
+
+    def enterDefactions(self, ctx):
+        pass
+
+    def enterTransact(self, ctx):
+        pass
+
+    def enterTransnoact(self, ctx):
+        pass
