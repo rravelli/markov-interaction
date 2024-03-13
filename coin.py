13ed3e5c0e322d9e9d28a4f1d1a2981cdc65f19e
ManonC12 <manon-c.coutier@wanadoo.fr>
2024-03-13 15:21:40 +0100

    coin 2.0

--- coin.py
+++ coin.py
@@ -47,20 +47,56 @@ class Simulation:
                 else:
                     return 6
 
-    def simulate(self):
-        while True:
-            val = self.iterate()
-            if val:
-                return val
+    def simulate(self, max_iter: int = None):
+        if max_iter is None:
+            while True:
+                val = self.iterate()
+                if val:
+                    return val
+        else:
+            for _ in range(max_iter):
+                val = self.iterate()
+                if val:
+                    return val
 
 
-def bernoulli(delta: float, epsilon: float, val=1):
+def monte_carlo(delta: float, epsilon: float, val=1):
     sum = 0
     N = int((log(2) - log(delta)) / (2 * epsilon) ** 2) + 1
     for _ in range(N):
-        sum += Simulation().simulate() == val
+        sum += Simulation().simulate(max_iter=5) == val
 
-    return sum / N
+    return sum / N, N
 
 
-print(bernoulli(0.05, 0.01, 2), 1 / 6)
+def sprt(theta: float, alpha: float = 0.01, beta: float = 0.01, epsilon: float = 0.01):
+    i = 1
+    k = 5
+    gamma1 = theta - epsilon
+    gamma0 = theta + epsilon
+    A = (1 - beta) / alpha
+    B = beta / (1 - alpha)
+
+    done = False
+    m = 0
+    Rm = 1
+
+    while not done:
+        val = Simulation().simulate(max_iter=k)
+        m += 1
+        if val == i:
+            Rm *= gamma1 / gamma0
+        else:
+            Rm *= (1 - gamma1) / (1 - gamma0)
+        if Rm >= A:
+            print(f"gamma<{gamma1}")
+            print(m)
+            return gamma1
+        if Rm <= B:
+            print(f"gamma>={gamma0}")
+            print(m)
+            return gamma0
+
+
+sprt(theta=0.1)
+print(monte_carlo(0.05, 0.01, 2), 1 / 6)
